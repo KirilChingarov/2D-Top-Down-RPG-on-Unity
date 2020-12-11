f0b344d81b942a747924343754b5d542aceee2b3
KirilChingarov <kiril.chingarov@gmail.com>
2020-12-11 12:14:21 +0200

    Added Player Controller Script

---
+++ Through the Dungeon/Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,51 @@
+﻿using Enums;
+using UnityEngine;
+
+namespace Character
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+        private CharacterAnimationController characterGFX;
+        private Direction currDirection = Direction.IDLE;
+
+        private Direction getDirectionFromSpeed(float horizontalSpeed, float verticalSpeed)
+        {
+            if (verticalSpeed > 0.01f)
+            {
+                return Direction.UP;
+            }
+            else if(verticalSpeed < -0.01f)
+            {
+                return Direction.DOWN;
+            }
+            else if (horizontalSpeed > 0.01f)
+            {
+                return Direction.RIGHT;
+            }
+            else if(horizontalSpeed < -0.01f)
+            {
+                return Direction.LEFT;
+            }
+            return Direction.IDLE;
+        }
+
+        public void setCharacterVelocity(float horizontalSpeed, float verticalSpeed, float moveSpeed)
+        {
+            rb.velocity = new Vector2(horizontalSpeed * moveSpeed, verticalSpeed * moveSpeed);
+        
+            currDirection = getDirectionFromSpeed(horizontalSpeed, verticalSpeed);
+            characterGFX.ChangeDirection(currDirection);
+        }
+
+        public void setRigidBody2D(Rigidbody2D newRb)
+        {
+            rb = newRb;
+        }
+
+        public void setCharacterAnimationContrller(CharacterAnimationController newChAnimationControllerC)
+        {
+            characterGFX = newChAnimationControllerC;
+        }
+    }
+}
---
+++ Through the Dungeon/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Character;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private float horizontalSpeed = 0f;
+        private float verticalSpeed = 0f;
+        public float moveSpeed = 5f;
+        private CharacterMovement characterMovement;
+        
+        private void Awake()
+        {
+            characterMovement = GetComponent<CharacterMovement>();
+            characterMovement.setRigidBody2D(GetComponent<Rigidbody2D>());
+            characterMovement.setCharacterAnimationContrller(GetComponentInChildren<CharacterAnimationController>());
+        }
+
+        private void FixedUpdate()
+        {
+            horizontalSpeed = Input.GetAxisRaw("Horizontal");
+            verticalSpeed = Input.GetAxisRaw("Vertical");
+            
+            characterMovement.setCharacterVelocity(horizontalSpeed, verticalSpeed, moveSpeed);
+        }
+    }
+}
