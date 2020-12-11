5da10f8374528e9b38b03ff6fc2055f5d8213260
KirilChingarov <kiril.chingarov@gmail.com>
2020-12-11 19:26:40 +0200

    Added database to keep movement stats

--- Through the Dungeon/Assets/Scripts/Player/PlayerController.cs
+++ Through the Dungeon/Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Character;
+using DatabasesScripts;
 using UnityEngine;
 
 namespace Player
@@ -8,14 +9,18 @@ namespace Player
     {
         private float horizontalSpeed = 0f;
         private float verticalSpeed = 0f;
-        public float moveSpeed = 5f;
+        public float moveSpeed = 1f;
         private CharacterMovement characterMovement;
+        private PlayerDatabaseConn dbConn;
         
         private void Awake()
         {
             characterMovement = GetComponent<CharacterMovement>();
             characterMovement.setRigidBody2D(GetComponent<Rigidbody2D>());
             characterMovement.setCharacterAnimationContrller(GetComponentInChildren<CharacterAnimationController>());
+            dbConn = new PlayerDatabaseConn("Player/CharacterMovement.db");
+            dbConn.setMoveSpeed(moveSpeed);
+            moveSpeed = dbConn.getPlayerMoveSpeed();
         }
 
         private void FixedUpdate()
