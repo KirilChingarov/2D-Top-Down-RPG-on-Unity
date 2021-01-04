32758c917a70f1f4d476698b8f5fadf0103775d1
KirilChingarov <kiril.chingarov@gmail.com>
2021-01-04 23:20:54 +0200

    Added character movement to enemy object

---
+++ Through the Dungeon/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+
+namespace Enemy
+{
+    
+    public class EnemyController : MonoBehaviour
+    {
+        private float horizontalSpeed = 0f;
+        private float verticalSpeed = 0f;
+        public float moveSpeed = 1f;
+        private CharacterMovement characterMovement;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            characterMovement = GetComponent<CharacterMovement>();
+            characterMovement.setRigidBody2D(GetComponent<Rigidbody2D>());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+}
