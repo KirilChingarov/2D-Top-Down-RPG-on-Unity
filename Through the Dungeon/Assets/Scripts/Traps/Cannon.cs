c82232b3293f9ada2436b85e509f6cb37868a23f
KirilChingarov <kiril.chingarov@gmail.com>
2021-01-23 14:41:18 +0200

    Created prefabs for cannon in 4 directions

--- Through the Dungeon/Assets/Scripts/Traps/Cannon.cs
+++ Through the Dungeon/Assets/Scripts/Traps/Cannon.cs
@@ -7,17 +7,19 @@ namespace Traps
     public class Cannon : MonoBehaviour
     {
         private GameObject projectile;
+        private Transform firePoint;
 
         private void Awake()
         {
             projectile = Resources.Load("Prefabs/Traps/Arrow") as GameObject;
+            firePoint = transform.Find("FirePoint").GetComponent<Transform>();
             
             InvokeRepeating("Shoot", 3f, new TrapsDatabaseConn("Cannon").getTrapCooldown());
         }
 
         private void Shoot()
         {
-            Instantiate(projectile, transform.position, transform.rotation);
+            Instantiate(projectile, firePoint.position, firePoint.rotation);
         }
     }
 }
