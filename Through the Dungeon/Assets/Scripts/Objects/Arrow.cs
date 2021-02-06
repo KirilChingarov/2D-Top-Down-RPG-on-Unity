b6985e6ea43794fb7541637150bbdf3deca122b8
KirilChingarov <kiril.chingarov@gmail.com>
2021-02-06 14:36:03 +0200

    Added level "Forest_3" and fixed Cannon trap

--- Through the Dungeon/Assets/Scripts/Objects/Arrow.cs
+++ Through the Dungeon/Assets/Scripts/Objects/Arrow.cs
@@ -10,13 +10,25 @@ namespace Objects
         private float m_ProjectileSpeed;
         private float m_ProjectileDamage;
         private Rigidbody2D m_Rb;
+        private float m_Range;
+        private Vector3 startingPosition;
 
         private void Awake()
         {
             m_Rb = GetComponent<Rigidbody2D>();
             m_ProjectileSpeed = new TrapsDatabaseConn("Cannon").GETTrapProjectileSpeed();
             m_ProjectileDamage = new TrapsDatabaseConn("Cannon").GETTrapDamage();
+            m_Range = new TrapsDatabaseConn("Cannon").GETTrapRange();
             m_Rb.velocity = m_Rb.velocity = transform.up * m_ProjectileSpeed;
+            startingPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(startingPosition, transform.position) > m_Range)
+            {
+                Destroy(gameObject);
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -24,10 +36,6 @@ namespace Objects
             if (other.gameObject.tag == "Player")
             {
                 other.gameObject.GetComponent<PlayerController>().TakeDamage(m_ProjectileDamage);
-                Destroy(gameObject);
-            }
-            else if (other.gameObject.tag == "Object")
-            {
                 Destroy(gameObject);
             }
         }
